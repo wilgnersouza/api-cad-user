4e7337fab07f521cd3b0b8f60ff7422883bc0426
wilgner souza <wilgner.gomes@mblabs.com.br>
2023-02-04 14:57:22 -0400

    feat: create services files for user entity

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,26 @@
+import AppError from '@shared/errors/AppError';
+import { IUserRequest } from '@shared/utils/generalDTOs';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UserRepository';
+
+export default class CreateUserService {
+  public async execute(data: IUserRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(data.email);
+
+    if (emailExists) {
+      throw new AppError('Email address already used.');
+    }
+
+    const user = usersRepository.create({
+      name: data.name,
+      email: data.email,
+      password: data.password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
